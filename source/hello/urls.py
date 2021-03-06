cdb68db361d18199abb9259a0363e120335633fe
karlygach <karygachkenenbaeva@gmail.com>
2021-03-06 13:36:43 +0600

    создана страница добавления новой записи с формой

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, note_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='note-list')
+    path('', index_view, name='note-list'),
+    path('note/add/', note_create_view, name='note-add')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from webapp.models import Note
+from webapp.models import Note, status_choices
+from webapp.forms import NoteForm
 
 
 def index_view(request):
     notes = Note.objects.all().filter(status='active').order_by('created_at', 'updated_at')
     return render(request, 'index.html', context={'notes': notes})
+
+
+def note_create_view(request):
+    if request.method == "GET":
+        form = NoteForm()
+        return render(request, 'note_create.html', context={'choices': status_choices, 'form': form})
+    elif request.method == "POST":
+        form = NoteForm(data=request.POST)
+        if form.is_valid():
+            note = Note.objects.create(
+                name=form.cleaned_data.get('name'),
+                email=form.cleaned_data.get('email'),
+                text=form.cleaned_data.get('text')
+            )
+            return redirect('note-list')
+        return render(request, 'note_create.html', context={'form': form})
 # Create your views here.
